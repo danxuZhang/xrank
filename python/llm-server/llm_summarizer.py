e5834a48d23937218628c3e5c7939126d6fff666
dxZhang <dxzhang49@gmail.com>
2024-04-19 20:34:28 +0800

    Use VLLM for reference

--- python/llm-server/llm_summarizer.py
+++ python/llm-server/llm_summarizer.py
@@ -1,16 +1,12 @@
-import torch
-from transformers import AutoTokenizer, AutoModelForCausalLM
-import deepspeed
+from vllm import LLM, SamplingParams
 
 class LlmSummarizer:
     def __init__(self, model_name="meta-llama/Llama-2-7b", device='cuda'):
+        self.sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
         self.device = device
-        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
-        self.model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.float16).to(self.device)
-        self.model = deepspeed.init_inference(self.model, mp_size=1, dtype=torch.float16, replace_with_kernel_inject=True)
+        self.llm = LLM(model="facebook/opt-125m")
 
     def summarize(self, text, max_length=50, num_beams=5):
-        inputs = self.tokenizer(text, return_tensors="pt").to(self.device)
-        summary_ids = self.model.generate(**inputs, max_length=max_length, num_beams=num_beams, early_stopping=True)
-        summary = self.tokenizer.decode(summary_ids[0], skip_special_tokens=True)
-        return summary
+        prompts =  f"Summarize the following content in ${max_length} words: {text}"
+        outputs = self.llm.generate(prompts, self.sampling_params)
+        return outputs
